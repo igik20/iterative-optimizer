da8c51b3793fae0715f7de71db12809c8294a2d7
Igor Trujnara <itrujnara@wp.pl>
2023-02-03 12:30:48 +0100

    Added code file with midpoint search

---
+++ search.py
@@ -0,0 +1,33 @@
+import math
+
+
+def f(x):
+    return 1 / x
+
+
+def midpoint_search(func, lower, upper, niter):
+    mid = (upper + lower) / 2
+    for i in range(niter):
+        eps = (upper - lower) / 10
+        
+        m_plus = mid + eps
+        m_minus = mid - eps
+        
+        f_plus = func(m_plus)
+        f_minus = func(m_minus)
+        
+        if f_plus > f_minus:
+            lower = mid
+        else:
+            upper = mid
+        mid = (upper + lower) / 2
+
+    return mid, func(mid)
+
+
+def main():
+    print(midpoint_search(f, -2, 2, 50))
+
+
+if __name__=="__main__":
+    main()
