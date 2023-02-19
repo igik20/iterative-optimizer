a114b4566ac2fff89fbc3a81c83ab7a28e3f0465
eviedimitrova <113925832+eviedimitrova@users.noreply.github.com>
2023-02-19 03:17:12 +0100

    Update optimizer.py

--- optimizer.py
+++ optimizer.py
@@ -1,15 +1,26 @@
 from __future__ import annotations
 from typing import Tuple, Callable, Any, Literal
+from golden_search.py import*
+from search.py import*
 
-def optimizer_alg(function: Callable[[Real, Any], Real],
-                          bounds: Tuple[Real, Real],
-                          epsilon: Real = 1e-5,
-                          type_optimization: Literal['min', 'max'] = 'min',
-                          max_iter: int = 500,
-                          verbose: bool = False,
-                          keep_history: bool = False,
-                          **kwargs) -> Tuple[Point, HistoryGSS]:
-
+Class Optimizer:
+  def __init__(self):
+    #not sure in the keys should be in quotation marks or not :(
+  dict = {"function": Callable[[Real, Any], Real],
+          "varname": Any,
+          "lower": Real,
+          "upper": Real,
+          "mode": Literal["Equal Interval", "Golden Search"] = "Golden Search",
+          "limittype": Literal["Absolute Tolerance:", "Relative Tolerance:", "Number of Iterations:"] = "Absolute Tolerance",
+          "limitval": Real}
+  mode = mode.lower().strip()
+  assert mode in ["Equal Interval", "Golden Search"], 'Invalid mode. Enter "Equal Interval" or "Golden Search"'
+  if mode == "Equal Interval" or mode == "equal interval":
+    return Modpoint_Search.midpoint_search(func, lower, upper, limitval) #not sure about the limitval here
+  else:
+    return G_Search.GOLDEN_search(func, lower, upper, limitval)
+    
+    
 
 
 '''
